5bc359e0d008d23855f47f6711d2098d798f3134
Sebastian Monroy <sebastian.monroy@gmail.com>
2015-01-09 11:47:13 -0500

    just getting started

---
+++ Assets/Scripts/GestureHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GestureHandler : Monobehaviour 
+{
+	public int fingerCount = 0;
+	public List<Vector2> fingerPositions = new List<Vector2>();
+
+	void Update() 
+	{
+
+	}
+}
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pad : MonoBehaviour 
+{
+	public Color color;
+	public float radius;
+
+	public Vector2 position;
+	public Vector2 velocity;
+
+	public bool isHeld;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
